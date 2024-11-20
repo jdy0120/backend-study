db1d089c2eeb4e1ac858318ee7ca6438819a07be
Doyeonism <jdy0120120@gmail.com>
2024-11-20 11:19:44 +0900

    update: dockerfile

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,18 @@
-import express from "express";
-import * as routes from "./routes";
+import { configDotenv } from "./configs/dotenv.config";
 import pool from "./configs/postgres";
+configDotenv();
 
-const app = express();
+const runServer = async () => {
+  const app = (await import("./configs/express.config")).default;
 
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+  // Start Server
+  const PORT = process.env.PORT;
+  app.listen(PORT, async () => {
+    await pool.connect();
+    console.log("PostgreSQL에 성공적으로 연결되었습니다!");
 
-// Routes
-app.use("/", routes.v1.test);
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+};
 
-// Start Server
-const PORT = 3000;
-app.listen(PORT, async () => {
-  await pool.connect();
-  console.log("PostgreSQL에 성공적으로 연결되었습니다!");
-
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+runServer();
